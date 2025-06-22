bb30145624013beaafdafe1ae322287b71e5f3bc
Tamizhjr <tamizhselvan22042003@gmail.com>
2025-06-23 01:38:52 +0530

    twoskips.py file added

---
+++ twoskips.py
@@ -0,0 +1,5 @@
+number = int(input("enter how many count do you want:"))
+table_number = int (input ("enter the table number:"))
+for i in range(1,number):
+    print(table_number,"*",i,"=",table_number*i)
+3
