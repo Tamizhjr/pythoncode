6947382c2c753df6afa6a65638088f3532f48c1d
Tamizhjr <tamizhselvan22042003@gmail.com>
2025-06-23 01:48:05 +0530

    modified oddeven.py file

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,5 @@
 verify_number = int(input("enter the number:"))
-if verfy_number % 2==0:
-  print(verfy_number,"is the even number")
+if verify_number % 2==0:
+  print(verify_number,"is the even number")
 else:
-  print(verfy_number,"is the odd number")
+  print(verify_number,"is the odd number")
